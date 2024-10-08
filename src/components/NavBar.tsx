2435971ee91d888d70d511c2eaae1a416475aa1a
mirek1546 <mirkolevo@gmail.com>
2024-10-08 11:02:00 +0200

    lôk

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,36 @@
-// src/components/NavBar.tsx
+import * as React from 'react';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import FolderIcon from '@mui/icons-material/Folder';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
+export default function LabelBottomNavigation() {
+  const [value, setValue] = React.useState('recents');
 
-import Typography from "@mui/material/Typography";
-import Conrainer from "@mui/material/Container";
-
-export const metadata = { title: "NavBar | ZoškaMeme" };
-
-export default function NavBar() {
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
   return (
-    <Conrainer>
-      <Typography> NavBar </Typography>
-    </Conrainer>
+    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
+      <BottomNavigationAction
+        label="Recents"
+        value="recents"
+        icon={<RestoreIcon />}
+      />
+      <BottomNavigationAction
+        label="Favorites"
+        value="favorites"
+        icon={<FavoriteIcon />}
+      />
+      <BottomNavigationAction
+        label="Nearby"
+        value="nearby"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    </BottomNavigation>
   );
 }
