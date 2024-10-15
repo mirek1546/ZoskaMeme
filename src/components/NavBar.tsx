36e94d733281d863fd38c8b32d5f75d522bc373f
Mirek1546 <mirkolevo@gmail.com>
2024-10-15 13:12:16 +0200

    navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,39 +1,56 @@
 // src/components/NavBar.tsx
-"use client";
+'use client';
 
-import * as React from 'react';
-import { BottomNavigation, BottomNavigationAction, Box } from '@mui/material';
-import HomeIcon from '@mui/icons-material/AccountCircle';
-import FolderIcon from '@mui/icons-material/Folder';
-import RestoreIcon from '@mui/icons-material/Restore';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import React, { useState } from 'react';
+import { BottomNavigation, BottomNavigationAction } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import LoginIcon from '@mui/icons-material/Login';
+import Box from '@mui/material/Box'; // Import missing
+import { useRouter } from 'next/navigation';
 
-export default function LabelBottomNavigation() {
-  const [value, setValue] = React.useState('recents');
+const NavBar: React.FC = () => {
+  const [value, setValue] = useState(0);
+  const router = useRouter();
 
-  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-    setValue(newValue);
+  const handleNavigation = (path: string) => {
+    router.push(path);
   };
 
   return (
-    <BottomNavigation sx={{ width: 500 }} value={value} onChange={handleChange}>
-      <BottomNavigationAction
-        label="Recents"
-        value="recents"
-        icon={<RestoreIcon />}
-      />
-      <BottomNavigationAction
-        label="Favorites"
-        value="favorites"
-        icon={<FavoriteIcon />}
-      />
-      <BottomNavigationAction
-        label="Nearby"
-        value="nearby"
-        icon={<LocationOnIcon />}
-      />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    </BottomNavigation>
+    <Box sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, width: '100%' }}>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        showLabels
+      >
+        <BottomNavigationAction
+          label="Domov"
+          icon={<HomeIcon />}
+          onClick={() => handleNavigation('/')}
+        />
+        <BottomNavigationAction
+          label="Príspevky"
+          icon={<PostAddIcon />}
+          onClick={() => handleNavigation('/pridat')}
+        />
+        <BottomNavigationAction
+          label="Registrácia"
+          icon={<PersonAddIcon />}
+          onClick={() => handleNavigation('/auth/registracia')}
+        />
+        <BottomNavigationAction
+          label="Prihlásenie"
+          icon={<LoginIcon />}
+          onClick={() => handleNavigation('/auth/prihlasenie')}
+        />
+      </BottomNavigation>
+    </Box>
   );
-}
+};
+
+export default NavBar;
+
